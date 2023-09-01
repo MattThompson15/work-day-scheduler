e5207082c471fd68d1e7a82c2ce51b945a8a198b
Matt Thompson <matthewthompson118@gmail.com>
2023-09-01 20:47:53 -0300

    modified script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//display time live with constant update IRT
+$("#currentDay"). text(moment().format(" MMMM Do YYYY, h:mm:ss a"));
+
+//this function displays the current date and time
+setInterval(function () {
+  $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+}, 1000)
+
+
+//variables being declared to create a container , select all of the buttons, Arrat time and numbers, 
+const createContainer = $(".container");
+const saveButton = document.querySelectorAll("button");
+const timeList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+const timeId = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+//a for loop to iterate through the timelist and timeId array and appends the data (cycles through and "pushes" appropriate content)
+//creates rows and a section for each time slot.
+for (let i = 0; i < timeList.length; i++) {
+  let createRow = $("<div class='row time-block'>").attr("id", timeId[i]);
+  let createTime = $("div class='hour col'>");
+}
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
