ff88e06d3c4e86a261dbe247c9c4fd71c1fbe3bc
Matt Thompson <matthewthompson118@gmail.com>
2023-09-06 11:39:51 -0300

    modified script.js, style.css

--- script.js
+++ script.js
@@ -1,95 +1,68 @@
-//display time live with constant update IRT
-$("#currentDay"). text(moment().format(" MMMM Do YYYY, h:mm:ss a"));
+$(document).ready(function () {
 
-//this function displays the current date and time
-setInterval(function () {
-  $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-}, 1000)
-
-
-//variables being declared to create a container , select all of the buttons, Arrat time and numbers, 
-const createContainer = $(".container");
-const saveButton = document.querySelectorAll("button");
-const timeList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-const timeId = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-//a for loop to iterate through the timelist and timeId array and appends the data (cycles through and "pushes" appropriate content)
-//creates rows and a section for each time slot.
-for (let i = 0; i < timeList.length; i++) {
-  let createRow = $("<div class='row time-block'>").attr("id", timeId[i]);
-  let createTime = $("div class='hour col'>");
-
-//Creates the text area to hold the content
-let createTextarea = $("<textarea class='col-10'>");
-createTextarea.attr("id", timeList[i]);
-//Will create the button and save icon using "fa-save using a font icon");
-let createButton = $("button type='button' class='saveBtn col-1 far fa-save'>");
-
-//this attaches the created row to the container
-createContainer.append(createRow);
-//this creates the timelist and attaches 8 rows 9am-5pm
-createTime.text(timeList[i]);
-createRow.append(createTime);
-
-//Attaches the previsously created "text area" so users can input content
-createRow.append(createTextarea);
-
-//creates buttons for each time slot (8am-5pm0
-createButton.text();
-createRow.append(createButton);
-}
-
-//calls the localStorage Function(will save all user input later in the JS)
-localStorageFunctions(); {
-
-  for(let index = 0; index < numbers.length; index++) {
-    $("textarea")[index].value = localStorage.getItem("textarea" + String(index + 1));
-
-  }
-}
-
-//this to save the stored data that has been entered into the textarea 
-$("button").on("click", function (event) {
-  event.preventDefault();
-
-  for (let index = 0; index < numbers.length; index++) {
-    localStorage.setItem('textarea' + String(index +1), $("textarea")[index].value)
-
-  }
-});
-
-//Colors the appropriate row/timeblock based upon whether or not the current time of day is ewual to less than the timeblock youre in
-//Grey= already past, Red = Current hour, Green= Coming up
-function timeValid() {
-  var currentHour =  moment().hours();
-  $(".time-block").each(function () {
-    var currentBlock = parseInt ($(this).attr("id").split(" ")[0]);
-
-    if (currentBlock < currentHour) {
-      $(this).addClass("past");
-    } else if (currentBlock === currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass ("future");
-
-    }
-  });
-}
-
-
-//Calls function to check whether time is still valid or not
-timeValid()
-    
-    
+  //Day, M/D/Y/T format
+    var init = false;
+    var now = moment().format('LLLL'); 
   
+    //12 hour clock format
+    let now24 = moment().format('H');
+
+    //24 hour clock format
+    let12 = moment().format('h');
+    if (init) {
+        now24 = 13;
+        now12 = 1;
+    }
+
+    //Display current date
+    const currentDate = $('#currentDay');
+    currentDate.text(now);
+
+    //init date and time
+    var cal = new Date();
+    var currentHour = cal.getHours();
+
+    for (var i = 8; i < 18; i++) {
+        if (i <currentHour) {
+           document.getElementById(i.toString()).classList.add('past');
+        } else if (i === currentHour) {
+           document.getElementById(i.toString()).classList.add('presmet');
+        } else if (i > currentHour) {
+          document.getElementById(i.toString()).classList.add('future');
+
+        }
+    }
+
+    //Local Storage Feature
+
+    {
+      $('.saveBtn').click(functionm() {
+        var hr8 = document.getElementById('8').value;
+                    localStorage.setItem('text8', hr8);
+                    var hr9 = document.getElementById('9').value;
+                    localStorage.setItem('text9', hr9);
+                    var hr10 = document.getElementById('10').value;
+                    localStorage.setItem('text10', hr10);
+                    var hr11 = document.getElementById('11').value;
+                    localStorage.setItem('text11', hr11);
+                    var hr12 = document.getElementById('12').value;
+                    localStorage.setItem('text12', hr12);
+                    var hr13 = document.getElementById('13').value;
+                    localStorage.set.Item('text13', hr13);
+                    var hr14 = document.getElementById('14').value;
+                    localStorage.setItem('text14', hr14);
+                    var hr15 = document.getElementById('15').value;
+                    localStorage.setItem('text15', hr15);
+                    var hr16 = document.getElementById('16').value;
+                    localStorage.setItem('text16', hr16);
+                    var hr17 = document.getElementById('17').value;
+                    localStorage.setItem('text17', hr17);
+      });
+    }
 
 
 
-
-
-
-
+  //Display saved tasks
+  var saved8 = localStorage.getItem('text8');
+  document.getElementById('8').value = saved8;
+  
