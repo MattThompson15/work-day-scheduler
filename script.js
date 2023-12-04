610bbfa8b8696f5c4cdd1d2060d0dbcbe11cbba7
Matt Thompson <matthewthompson118@gmail.com>
2023-12-04 18:43:31 -0400

    finished javascript

--- script.js
+++ script.js
@@ -34,4 +34,25 @@ function refreshColor() {
           }
         });
     }   
+    //This willget user input from localStorage and set textarea values for each time block
+    $('.time-block').each(function() {
+        const key = $(this).attr('id');
+        const value = localStorage.getItem(key);
+        $(this).children('.description').val(value);
+    });
+    //Function that updates time
+    function updateTime() {
+        const dateElement = $('#date');
+        const timeElement = $('#time');
+        const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+        const currentTime = dayjs().format('hh:mm:ss A');
+        dateElement.text(currentDate);
+        timeElement.text(currentTime);
+    }
+    //Calls the three main functions to set up the page
+    hourlyColor();
+    textEntry();
+    refreshColor();
+    setInterval(updateTime, 1000);
+});
     
